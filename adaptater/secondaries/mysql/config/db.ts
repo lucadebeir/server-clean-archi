49d56edff7b15654d756bfa0a69a93d4bc1583b8
lucadebeir <l.debeir@me.com>
2021-03-24 20:24:27 +0100

    Create db.ts

---
+++ adaptater/secondaries/mysql/config/db.ts
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+import { getEnvironment } from "./environmentFile";
+
+const environment: any = getEnvironment();
+
+const sequelize = new Sequelize(
+  environment.DB_NAME,
+  environment.DB_USER,
+  environment.DB_PASSWORD,
+  {
+    host: environment.HOST,
+    port: environment.PORT ? environment.PORT : null,
+    dialect: "mysql",
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
+
+const db = {
+  sequelize: sequelize,
+};
+
+export = db;
